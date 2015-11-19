bc3b35647c54c5909378ccc5e38a4ca6ab568d86
LIAO Zhenyu <102982liaozy@tongji.edu.cn>
2015-11-20 00:50:02 +0800

    1. add merge words function in NoiseWiper 2. test unfinished

---
+++ src/entity/DictionaryItem.java
@@ -0,0 +1,46 @@
+package entity;
+
+import java.util.List;
+
+/**
+ * Created by Jaric Liao on 2015/11/20.
+ */
+public class DictionaryItem {
+    // 合并模式：WL 词语表 或者 RE 正则表达式
+    private String mode;
+    private String pattern;
+    private List<String> wordsList;
+    private String mergeTo;
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public List<String> getWordsList() {
+        return wordsList;
+    }
+
+    public void setWordsList(List<String> wordsList) {
+        this.wordsList = wordsList;
+    }
+
+    public String getMergeTo() {
+        return mergeTo;
+    }
+
+    public void setMergeTo(String mergeTo) {
+        this.mergeTo = mergeTo;
+    }
+}
--- src/util/NoiseWiper.java
+++ src/util/NoiseWiper.java
@@ -1,8 +1,16 @@
 package util;
 
 import entity.Danmaku;
+import entity.DictionaryItem;
+import entity.Word;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Jaric Liao on 2015/11/16.
@@ -11,6 +19,8 @@ public class NoiseWiper {
 
     private static Set<String> punctuation = new HashSet<String>(Arrays.asList(",",".","!","?","/","\\",":",";","...","=","-","←","#","@","$","%","`","*","&","(",")",
                                                                             "，","。","！","？","/","：","；","。。。","……","（","）"));
+    private static String DICTIONARY_PATH = ".//data//dict.json";
+    private static List<DictionaryItem> dictionary;
 
     public static String replace( String rawContent ){
         for ( String s : punctuation ){
@@ -19,15 +29,41 @@ public class NoiseWiper {
         return rawContent;
     }
 
-    public static List<Danmaku> removeDuplicate( List<Danmaku> danmakuList ){
-        List<Danmaku> noDupList = new ArrayList<Danmaku>();
-        Danmaku lastDanmaku = null;
-        for ( Danmaku danmaku : danmakuList ){
-            if (( lastDanmaku == null ) || (!lastDanmaku.getSenderId().equals(danmaku.getSenderId())) || (!lastDanmaku.getContent().equals(danmaku.getContent()))) {
-                noDupList.add(danmaku);
-                lastDanmaku = danmaku;
+    public static void dict_init() {
+        try {
+            File file = new File(DICTIONARY_PATH);
+            FileInputStream inputStream = new FileInputStream(file);
+            InputStreamReader inputReader = new InputStreamReader(inputStream, "UTF-8");
+            BufferedReader bufferedReader = new BufferedReader(inputReader);
+            String output;
+            String json = "";
+            while ((output = bufferedReader.readLine()) != null) {
+                json = json + output;
+            }
+            bufferedReader.close();
+            inputReader.close();
+            inputStream.close();
+            dictionary = JsonUtil.toObjectList(json,DictionaryItem.class);
+        } catch ( Exception e ){
+            e.printStackTrace();
+        }
+    }
+
+    public static String merge(Word word){
+        String content = word.getCont();
+        for ( DictionaryItem dictionaryItem : dictionary ){
+            if ( dictionaryItem.getMode().equals("WL") ){
+                List<String> wordlist = dictionaryItem.getWordsList();
+                if ( wordlist.contains(word) ) return dictionaryItem.getMergeTo();
+                else return content;
+            }
+            else if ( dictionaryItem.getMode().equals("RE")){
+                Pattern pattern = Pattern.compile(dictionaryItem.getPattern());
+                Matcher matcher = pattern.matcher(content);
+                if ( matcher.matches() ) return dictionaryItem.getMergeTo();
+                else return content;
             }
         }
-        return noDupList;
+        return content;
     }
 }
